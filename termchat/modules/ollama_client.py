e9a512d9f5a5eeabf637cbf98134441bff2ef32e
SmashedAdam <adam.sy.yeung@gmail.com>
2025-08-16 00:19:20 +0800

    WIP: refactoring code to modules

---
+++ termchat/modules/ollama_client.py
@@ -0,0 +1,44 @@
+import ollama
+from rich import print # debug use only
+
+
+
+class ollama_client:
+    def __init__(self, host):
+        # spawns an ollama client
+        self.host = host
+        self.client = ollama.Client(host) # create a client object
+        models_raw = self.client.list()
+        self.models = [] # wait ollama instance to return model list
+        self.chat_history=[] # use to handle interactive chat
+        for model in models_raw["models"]:
+            self.models.append(model["model"])
+    
+    def chat(self):
+        """send chat request to ollama(context aware).
+        """
+        pass
+
+    def generate(self, model, prompt): 
+        """send chat request to ollama(context free).
+        
+        """
+        response = self.client.generate(model, prompt)
+        return response
+
+    def show(self):
+        """inspect a model and make it into a dictionary. Omit modelfile, license and template since it's quite uselsss.
+        """
+        self.info = self.client.show(self.models[0])
+        # self.info.pop("modelfile")
+        self.info_new = {}
+        for item in self.info:
+            self.info_new[item[0]] = item[1]
+        self.info_new.pop("modelfile")
+        self.info_new.pop("license")
+        self.info_new.pop("template")
+        return self.info_new
+
+ola = ollama_client("http://100.64.60.10:11434")
+
+print(ola.show())
